e526f3c8536f1fca0e0cd9c9748eea37fcfdcd96
Aiken Cairncross <acairncross@gmail.com>
2023-11-08 00:56:26 +0000

    Day 1 part 2

--- Day01.lean
+++ Day01.lean
@@ -1,19 +1,37 @@
-import «Aoc2018»
+import Aoc2018
+import Std.Data.HashMap
 
-def solvePart1 (input : String) := Id.run do
-  let mut frequency : Int := 0
-
-  for line in input.splitOn "\n" do
-    if line.length == 0 then continue
+def parseInput (input : String) : List Int :=
+  input
+  |>.splitOn "\n"
+  |> List.filter (fun (line : String) => line.length != 0)
+  |> List.map (fun (line : String) =>
     let sign := match String.front line with
     | '+' => 1
     | _ => -1
     let magnitude := (String.drop line 1).toInt!
-    let n := sign * magnitude
+    sign * magnitude)
+
+def solvePart1 (changes : List Int) := Id.run do
+  let mut frequency : Int := 0
+  for n in changes do
     frequency := frequency + n
+  return frequency
+
+def solvePart2 (changes : List Int) := Id.run do
+  let mut frequency : Int := 0
+  let mut seen_frequencies : Std.HashMap Int Unit := Std.mkHashMap
+  let mut i : Nat := 0
+
+  while not (seen_frequencies.contains frequency) do
+    seen_frequencies := seen_frequencies.insert frequency ()
+    frequency := frequency + changes[i]!
+    i := (i + 1) % changes.length
 
   return frequency
 
 def main : IO Unit := do
   let input <- IO.FS.readFile "inputs/day01.txt"
-  IO.println s!"{solvePart1 input}"
+  let changes := parseInput input
+  IO.println s!"{solvePart1 changes}"
+  IO.println s!"{solvePart2 changes}"
--- lakefile.lean
+++ lakefile.lean
@@ -1,11 +1,14 @@
 import Lake
 open Lake DSL
 
+require std from git
+  "https://github.com/leanprover/std4/" @ "v4.2.0"
+
 package «aoc-2018» where
   -- add package configuration options here
 
-lean_lib «Aoc2018» where
+lean_lib Aoc2018 where
   -- add library configuration options here
 
-lean_exe «day01» where
+lean_exe day01 where
   root := `Day01
