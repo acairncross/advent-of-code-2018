e9cddceaf5dd1900aa584f22dc3af00f8daf4ba6
Aiken Cairncross <acairncross@gmail.com>
2023-11-19 20:41:11 +0000

    Add theorem Array.size_append

--- Aoc2018/Lib.lean
+++ Aoc2018/Lib.lean
@@ -1,3 +1,5 @@
+import Std.Data.Array.Init.Lemmas
+
 namespace IO.FS.Stream
 
 partial def readlines (stream : IO.FS.Stream) : IO (List String) := do
@@ -14,3 +16,13 @@ end IO.FS.Stream
 -- There isn't a Hashable instance for Char in the stdlib currently
 instance : Hashable Char where
   hash c := c.val.toUInt64
+
+namespace Array
+
+theorem size_append (xs : Array α) (ys : Array α) :
+  (xs ++ ys).size = xs.size + ys.size := by
+    unfold size
+    rw [Array.append_data]
+    rw [List.length_append]
+
+end Array
